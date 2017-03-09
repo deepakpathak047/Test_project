c0bd4b89a85631e4a61ef4e6a85d12ebf56d5094
deepakp <deepakpathak047@gmail.com>
2017-03-09 13:29:21 +0530

    deepak

--- app/src/main/java/com/example/deepakp/test_project/activities/ContactTabActivity.java
+++ app/src/main/java/com/example/deepakp/test_project/activities/ContactTabActivity.java
@@ -15,6 +15,8 @@ public class ContactTabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
